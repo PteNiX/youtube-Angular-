82d3d693863b1ba4d796cedfa4543b52605c1e84
Aliaksandr Ptsitsa <ptenix@gmail.com>
2022-09-30 17:05:37 +0300

    feat: add new modules in modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+   
   ],
   imports: [
     BrowserModule,
