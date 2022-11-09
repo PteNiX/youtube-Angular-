44e679bcd655919aff5d1e7a2c861c7da84c820b
Aliaksandr Ptsitsa <ptenix@gmail.com>
2022-11-09 17:44:43 +0300

    feat: show the "Login" and "Logout"

--- src/app/core/components/header/login/login.component.ts
+++ src/app/core/components/header/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,15 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   loginStorage: any = 'Your Name';
 
   loginPath = './assets/images/login.png';
 
+  Login: string = 'Login';
+
+  buttonLogin: any;
+
   ngOnInit(): void {
+    this.buttonLogin = document.querySelector('.logout');
+
     if (sessionStorage.getItem('login') && sessionStorage.getItem('password')) {
       this.loginStorage = sessionStorage.getItem('login');
+      this.Login = 'Logout';
+      this.buttonLogin.removeAttribute('disabled');
+    } else {
+      this.buttonLogin.setAttribute('disabled', true);
+      this.router.navigate(['./login']);
     }
+  }
+
+  logout() {
+    sessionStorage.removeItem('login');
+    sessionStorage.removeItem('password');
+    this.Login = 'Logout';
+    window.location.reload();
   }
 }
