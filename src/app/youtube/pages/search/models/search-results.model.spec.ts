79fbbd56490e144d63538f4d93b948d384763e4e
Aliaksandr Ptsitsa <ptenix@gmail.com>
2022-12-30 13:11:13 +0300

    feat: add build with sort and color change

--- src/app/youtube/pages/search/models/search-results.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SearchResults } from './search-results.model';
-
-describe('SearchResults', () => {
-  it('should create an instance', () => {
-    expect(new SearchResults()).toBeTruthy();
-  });
-});
--- src/app/youtube/pages/search/models/search-youtube.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SearchYoutube } from './search-youtube.model';
-
-describe('SearchYoutube', () => {
-  it('should create an instance', () => {
-    expect(new SearchYoutube()).toBeTruthy();
-  });
-});
