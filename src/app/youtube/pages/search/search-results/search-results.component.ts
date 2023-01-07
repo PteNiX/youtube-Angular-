4246cf6f05d8b1613b07640931eef631f2ca54e4
Aliaksandr Ptsitsa <ptenix@gmail.com>
2023-01-07 17:46:55 +0300

    feat: add inscription 'try to search'

--- src/app/youtube/pages/search/search-results/search-results.component.ts
+++ src/app/youtube/pages/search/search-results/search-results.component.ts
@@ -1,5 +1,8 @@
+import { style } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { DataService } from 'src/app/core/services/data.service';
 
 @Component({
   selector: 'app-search-results',
@@ -7,16 +10,36 @@ import { Router } from '@angular/router';
   styleUrls: ['./search-results.component.scss'],
 })
 export class SearchResultsComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private shared: DataService,) {}
 
   mockResponse: any;
 
+  mainSearch: any;
+
+  hide() {
+    if (sessionStorage.getItem('mainInput')) {
+      document.querySelector('.main-title')?.classList.add('main-hidden');
+    }
+  }
+
   ngOnInit(): void {
+    this.mainSearch = this.shared.getMessage();
     if (
       !sessionStorage.getItem('login') &&
       !sessionStorage.getItem('password')
     ) {
       this.router.navigate(['./login']);
     }
+
+    const obsUsingConstructor = new Observable((observer) => {
+      const search2 = document.querySelector('.search-input');
+
+      search2?.addEventListener('input', (event) => {
+        observer.next(event);
+        sessionStorage.setItem('mainInput', this.mainSearch.text);
+      });
+    });
+
+    obsUsingConstructor.subscribe(() => this.hide());
   }
 }
